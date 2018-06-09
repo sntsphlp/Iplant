559ec36ec51f414249f020a07716b025e7eaebda
klenner1 <klenne.al@gmail.com>
2018-06-09 14:24:24 -0300

    LaunchActivity

--- app/src/main/java/puc/iot/com/iplant/LaunchActivity.java
+++ app/src/main/java/puc/iot/com/iplant/LaunchActivity.java
@@ -2,9 +2,12 @@ package puc.iot.com.iplant;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -13,34 +16,42 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+
 
 public class LaunchActivity extends AppCompatActivity {
 
     private final static int RC_SIGN_IN = 0;
+    private static final String TAG = "LaunchActivity";
     private GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth mAuth;
-
+    private ImageView imageViewAppIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        mAuth = FirebaseAuth.getInstance();
 
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account!=null){
-            Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
-            startActivity(intent);
-            LaunchActivity.this.finish();
+        imageViewAppIcon =findViewById(R.id.imageViewAppIcon);
+        imageViewAppIcon.setAlpha(0f);
+        imageViewAppIcon.setScaleX(0f);
+        imageViewAppIcon.setScaleY(0f);
+        imageViewAppIcon.animate().alpha(1f).scaleY(1f).scaleX(1f).setDuration(500);
+        if(mAuth!=null){
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            logged(currentUser,true);
         }else {
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestIdToken(getString(R.string.default_web_client_id))
                     .requestEmail()
                     .build();
-            mAuth = FirebaseAuth.getInstance();
-            FirebaseUser currentUser = mAuth.getCurrentUser();
-
             mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
             SignInButton signInButton = findViewById(R.id.sign_in_button);
             findViewById(R.id.textViewLogin).setVisibility(View.VISIBLE);
@@ -56,34 +67,56 @@ public class LaunchActivity extends AppCompatActivity {
         }
     }
 
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                fireBaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Log.w(TAG, "Google sign in failed", e);
+            }
         }
     }
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            if (account!=null){
-                Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
-                startActivity(intent);
-                LaunchActivity.this.finish();
-            }else {
-                Toast.makeText(this,"Error",Toast.LENGTH_LONG).show();
-            }
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.e("Login", "signInResult:failed code=" + e.getStatusCode());
-            Toast.makeText(this,"Error",Toast.LENGTH_LONG).show();
+
+    private void fireBaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "fireBaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            logged(user,false);
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LaunchActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void logged(FirebaseUser user,boolean wait) {
+        int waitTime=0;
+        if (wait){
+            waitTime=1500;
+        }
+        if (user!=null) {
+            Handler handle = new Handler();
+            handle.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    LaunchActivity.this.finish();
+                }
+            }, waitTime);
         }
     }
 }
